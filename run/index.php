008f34910aeb41f709e45704f1df854bbb9aeb11
JanKolo04 <jankolodziej04@gmail.com>
2022-05-21 23:09:47 +0200

    ideas for fetching data

---
+++ run/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<title>Test</title>
+</head>
+<body>
+
+	<script type="text/javascript">
+		
+
+		//1. First option
+		/*
+		const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+		async function getDataFromApi(URL) {
+			const response = await fetch(URL);
+			var data = await response.json();
+
+			console.log(data)
+		}
+
+		getDataFromApi(URL);
+		*/
+
+
+		//2. Second option with getJSON 
+		//sprawdzic czy wyszuka nam z róznymi nazwami zmiennych
+
+
+		/*
+			NEEDED LOCATIONS IN API
+
+			1. IMG pokemon --sprites--
+
+		*/
+
+	</script>
+
+</body>
+</html>
